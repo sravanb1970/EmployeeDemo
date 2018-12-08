dcf8545c73c75c09491f444efc5b05d5684af511
sravanb1970 <42944182+sravanb1970@users.noreply.github.com>
2018-12-09 02:40:49 +0530

    refactored code in client app

--- src/main/java/com/citibank/client/App.java
+++ src/main/java/com/citibank/client/App.java
@@ -1,6 +1,5 @@
 package com.citibank.client;
 
-
 import java.sql.Date;
 
 import org.springframework.context.support.AbstractApplicationContext;
@@ -14,32 +13,44 @@ import com.citibank.serviceImpl.EmployeeServiceImpl;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-  AbstractApplicationContext con=new FileSystemXmlApplicationContext("src/main/java/com/citibank/cfgs/applicationContext.xml");
+public class App {
+	public static void main(String[] args) {
+		AbstractApplicationContext con = new FileSystemXmlApplicationContext(
+				"src/main/java/com/citibank/cfgs/applicationContext.xml");
 
-  Employee e=new Employee();
-  e.setFirstName("Mike");
-  e.setLastName("Thomas");
-    e.setDoj(new Date(0));
-    e.setGender("Male");
-    e.setSalary("80000");
-    e.setEmail("mikethos@gmail.com");
-EmployeeService es=con.getBean("employeeService", EmployeeServiceImpl.class);
-//es.createEmployee(e);
-Employee emp = es.getEmployeeById(1);
-System.out.println(emp.getFirstName());
-System.out.println(emp.getLastName());
-System.out.println(emp.getEmail());
-System.out.println(emp.getGender());
-System.out.println(emp.getSalary());
-System.out.println(emp.getDoj());
-System.out.println();
+		createEmployee();
+		EmployeeService es = con.getBean("employeeService", EmployeeServiceImpl.class);
+		employeeExtracted(es);
 
-System.out.println("Created successfully");
-    
-    
-    }
+		System.out.println("Created successfully");
+
+	}
+
+	/**
+	 * @param es
+	 * created for adding the employee details from e object and printed the values
+	 */
+	private static void employeeExtracted(EmployeeService es) {
+		Employee emp = es.getEmployeeById(1);
+		System.out.println(emp.getFirstName());
+		System.out.println(emp.getLastName());
+		System.out.println(emp.getEmail());
+		System.out.println(emp.getGender());
+		System.out.println(emp.getSalary());
+		System.out.println(emp.getDoj());
+		System.out.println();
+	}
+
+	/**
+	 * this is extracted method from employee
+	 */
+	private static void createEmployee() {
+		Employee e = new Employee();
+		e.setFirstName("Mike");
+		e.setLastName("Thomas");
+		e.setDoj(new Date(0));
+		e.setGender("Male");
+		e.setSalary("80000");
+		e.setEmail("mikethos@gmail.com");
+	}
 }
